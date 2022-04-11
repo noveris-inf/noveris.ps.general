3055271bc6ed6de4240ae3e12303a7b87d24dac3
Jesse Reichman <Jesse.Reichman@noveris.com.au>
2022-04-11 16:06:34 +0930

    Updated for import to archmachina.

---
+++ entrypoint.ps1
@@ -0,0 +1,49 @@
+<#
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Profile
+)
+
+$ErrorActionPreference = "Stop"
+. ./ps-cibootstrap/bootstrap.ps1
+
+########
+# Capture version information
+$version = @($Env:GITHUB_REF, "v0.1.0") | Select-ValidVersions -First -Required
+
+Write-Information "Version:"
+$version
+
+########
+# Build stage
+Invoke-CIProfile -Name $Profile -Steps @{
+
+    lint = @{
+        Script = {
+            Use-PowershellGallery
+            Install-Module PSScriptAnalyzer -Scope CurrentUser
+            Import-Module PSScriptAnalyzer
+            $results = Invoke-ScriptAnalyzer -IncludeDefaultRules -Recurse .
+            if ($null -ne $results)
+            {
+                $results
+                Write-Error "Linting failure"
+            }
+        }
+    }
+
+    pr = @{
+        # Dependencies = $("lint")
+    }
+
+    latest = @{
+        # Dependencies = $("lint")
+    }
+
+    release = @{
+    }
+}
